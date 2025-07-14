0430c42a3e93dfb2a91c3afa14de0d8bd4281c93
Mariusz Kogen <k0gen@pm.me>
2025-07-14 13:59:38 +0200

    add alpha tag

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,2 @@
-export { v1_23_16 as current } from './v1.23.16'
+export { v1_23_16_1 as current } from './v1.23.16.1-alpha.0'
 export const other = []
--- startos/install/versions/v1.23.16.1-alpha.0.ts
+++ startos/install/versions/v1.23.16.1-alpha.0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v1_23_16 = VersionInfo.of({
-  version: '1.23.16:1.0',
+export const v1_23_16_1 = VersionInfo.of({
+  version: '1.23.16:1-alpha.0',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
