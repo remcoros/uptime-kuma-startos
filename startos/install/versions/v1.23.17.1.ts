24fcce47ba187b886b4f390a14b70d17d0647de0
Remco Ros <remcoros@live.nl>
2025-12-18 09:45:02 +0000

    bump version

--- startos/install/versions/v1.23.17.1.ts
+++ startos/install/versions/v1.23.17.1.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v1_23_17_1 = VersionInfo.of({
-  version: '1.23.17:1-beta.0',
+  version: '1.23.17:1-beta.1',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
